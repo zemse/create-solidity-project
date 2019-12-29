49df24d75f35a9ea1c8829edefdb87af5e164b22
zemse <sohamzemse143@gmail.com>
2019-12-30 00:12:12 +0530

    skipping compile step if contracts not changed

--- compile.js
+++ compile.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const fs = require('fs-extra');
 const solc = require('solc');
+const ethers = require('ethers');
 
 const sourceFolderPath = path.resolve(__dirname, 'contracts');
 const buildFolderPath = path.resolve(__dirname, 'build');
+const lastSourceHashFilePath = path.resolve(__dirname, 'sst-config.json');
 
 const getContractSource = contractFileName => {
   const contractPath = path.resolve(__dirname, 'contracts', contractFileName);
@@ -21,64 +23,90 @@ fs.readdirSync(sourceFolderPath).forEach(contractFileName => {
     }
   }
 });
-//console.log(sources);
+// console.log(sources);
 
-const input = {
-    language: 'Solidity',
-    sources,
-    settings: {
-        outputSelection: {
-            '*': {
-                '*': [ '*' ]
-            }
-        }
-    }
+function convertToHex(inputString) {
+  var hex = '';
+  for(var i=0;i<inputString.length;i++) {
+    hex += ''+inputString.charCodeAt(i).toString(16);
+  }
+  return hex;
 }
 
-console.log('\n'.repeat(process.stdout.rows)+'Compiling contracts...');
-const output = JSON.parse(solc.compile(JSON.stringify(input)));
-console.log('Done');
+const sourceHash = ethers.utils.sha256('0x'+convertToHex(JSON.stringify(sources)));
 
-let shouldBuild = true;
+console.log('\n'.repeat(process.stdout.rows));
 
-if (output.errors) {
-  // console.error(output.errors);
-  for(error of output.errors) {
-    console.log('-'.repeat(process.stdout.columns));
-    console.group(error.severity.toUpperCase());
-    console.log(error.formattedMessage);
-    console.groupEnd();
-  }
-  if(Object.values(output.errors).length) console.log('-'.repeat(process.stdout.columns));
-  // throw '\nError in compilation please check the contract\n';
-  for(error of output.errors) {
-    if(error.severity === 'error') {
-      shouldBuild = false;
-      throw 'Error found\n';
-      break;
-    }
-  }
-}
-
-if(shouldBuild) {
-  console.log('\nBuilding please wait...');
-
-  fs.removeSync(buildFolderPath);
-  fs.ensureDirSync(buildFolderPath);
-
-  let i = 0;
-  for (let contractFile in output.contracts) {
-    for(let key in output.contracts[contractFile]) {
-      //console.log(key, Object.keys(output.contracts[contractFile][key]));
-      fs.outputJsonSync(
-        path.resolve(buildFolderPath, `${contractFile.split('.')[0]}_${key}.json`),
-        output.contracts[contractFile][key]
-      );
-
-    }
-    i++;
-  }
-  console.log('Build finished successfully!\n');
+if(fs.existsSync(buildFolderPath)
+  && fs.existsSync(lastSourceHashFilePath)
+  && (JSON.parse(fs.readFileSync(lastSourceHashFilePath, 'utf8'))).sourceHash === sourceHash) {
+  console.log('No changes in .sol files detected... \nSkiping compile script...\n');
 } else {
-  console.log('\nBuild failed\n');
+  // write the source hash there at the end of
+  // console.log(lastSourceHash,sourceHash);
+
+  const input = {
+      language: 'Solidity',
+      sources,
+      settings: {
+          outputSelection: {
+              '*': {
+                  '*': [ '*' ]
+              }
+          }
+      }
+  }
+
+  console.log('Compiling contracts...');
+  const output = JSON.parse(solc.compile(JSON.stringify(input)));
+  console.log('Done');
+
+  let shouldBuild = true;
+
+  if (output.errors) {
+    // console.error(output.errors);
+    for(error of output.errors) {
+      console.log('-'.repeat(process.stdout.columns));
+      console.group(error.severity.toUpperCase());
+      console.log(error.formattedMessage);
+      console.groupEnd();
+    }
+    if(Object.values(output.errors).length) console.log('-'.repeat(process.stdout.columns));
+    // throw '\nError in compilation please check the contract\n';
+    for(error of output.errors) {
+      if(error.severity === 'error') {
+        shouldBuild = false;
+        throw 'Error found\n';
+        break;
+      }
+    }
+  }
+
+  if(shouldBuild) {
+    console.log('\nBuilding please wait...');
+
+    fs.removeSync(buildFolderPath);
+    fs.ensureDirSync(buildFolderPath);
+
+    let i = 0;
+    for (let contractFile in output.contracts) {
+      for(let key in output.contracts[contractFile]) {
+        //console.log(key, Object.keys(output.contracts[contractFile][key]));
+        fs.outputJsonSync(
+          path.resolve(buildFolderPath, `${contractFile.split('.')[0]}_${key}.json`),
+          output.contracts[contractFile][key]
+        );
+
+      }
+      i++;
+    }
+    console.log('Build finished successfully!\n');
+  } else {
+    console.log('\nBuild failed\n');
+  }
+
+  fs.outputJsonSync(
+    path.resolve(lastSourceHashFilePath),
+    { sourceHash }
+  );
 }
