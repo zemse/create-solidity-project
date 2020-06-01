b67b0cb180b2f42926cd1f97b7725254b7bfaf55
zemse <sohamzemse143@gmail.com>
2020-06-02 02:52:55 +0530

    add SPDX-License-Identifier to demo contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 // Credit: This contract is copied and pasted from https://docs.ethers.io/ethers.js/html/api-contract.html. Some necessary modifications for 0.5.11 have been done.
 
 // Place your solidity files in this contracts folder and run the compile.js file using node compile.js file in project directory to compile your contracts.
