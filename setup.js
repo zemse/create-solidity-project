d5536436a30cc01b60dfbe65016e936174bc3f0e
zemse <sohamzemse143@gmail.com>
2020-08-04 02:24:21 +0530

    Add installation description

--- setup.js
+++ setup.js
@@ -20,21 +20,21 @@ if (argv.template || argv.t) {
 }
 
 const name = argv._[0];
-const initiating_text = [
-  '\nInitiating',
-  'Create Solidity Project'.cyan.underline,
-];
+let initiating_text = `\nInitiating ${
+  'Create Solidity Project'.cyan.underline
+}`;
 if (templateName !== 'JavaScript') {
-  initiating_text.push('with', templateName.cyan.underline);
+  initiating_text += ` with ${templateName.cyan.underline}`;
 }
-console.log(...initiating_text, '...');
+console.log(initiating_text + ':\n');
 
 templateName = templateName.toLowerCase();
 const template = require(`./templates/${templateName}/setup.json`);
 
-console.log(`\nCreating`, `${name}`.green, `directory and installing files...`);
+process.stdout.write(
+  `Creating ${name.green} directory and installing files...`
+);
 fs.mkdirSync(path.resolve(process.cwd(), name));
-
 template.files.forEach((filePath) => {
   const from = filePath.from.split('/');
   const to = filePath.to ? filePath.to.split('/') : from;
@@ -55,39 +55,53 @@ fs.writeFileSync(
   JSON.stringify(packageJson, null, 2),
   { encoding: 'utf8' }
 );
+process.stdout.clearLine(0);
+process.stdout.cursorTo(0);
+process.stdout.write(`Created ${name.green} directory and installed files.\n`);
 
 const { execSync } = require('child_process');
-console.log('\nInstalling dependencies...');
-execSync(
-  `npm i ${template.install.map((p) => `${p.name}@${p.version}`).join(' ')}`,
-  {
-    cwd: path.resolve(process.cwd(), name),
-    stdio: 'ignore',
-  }
-);
-execSync(
-  `npm i --save-dev ${template.install_dev
-    .map((p) => `${p.name}@${p.version}`)
-    .join(' ')}`,
-  {
-    cwd: path.resolve(process.cwd(), name),
-    stdio: 'ignore',
-  }
-);
 
-if (packageJson.scripts.postinstall) {
-  execSync(`${packageJson.scripts.postinstall}`, {
+const execSyncSilent = (command) =>
+  execSync(command, {
     cwd: path.resolve(process.cwd(), name),
     stdio: 'ignore',
   });
+
+const packageNames = template.packages.map((p) => p.name);
+
+template.packages.forEach((package, i) => {
+  let text = `Installing ${packageNames.join(', ')}...`;
+  if (text.length > process.stdout.columns) {
+    text = `Installing ${packageNames[i]} ${
+      `(${
+        packageNames.slice(i).length === 0
+          ? 'final remaining'
+          : `${packageNames.slice(i).length} more remaining`
+      })`.grey
+    }`;
+  }
+  process.stdout.write(text);
+  execSyncSilent(
+    `npm i ${package.name}@${package.version}${
+      package.name === 'dev' ? ' --save--dev' : ''
+    }`
+  );
+  process.stdout.clearLine(0);
+  process.stdout.cursorTo(0);
+  packageNames[i] = packageNames[i].green;
+});
+
+process.stdout.write(`Installed ${packageNames.join(', ')}.\n`);
+
+if (packageJson.scripts.postinstall) {
+  execSyncSilent(`${packageJson.scripts.postinstall}`);
 }
 
-console.log('\nInitiating Git Repository...');
-execSync(
-  `cd ${name} && git init && git add . && git commit -m "Initial commit"`,
-  () => {}
-);
-console.log('Done!');
+process.stdout.write(`Initiating Git Repository...`);
+execSyncSilent(`git init && git add . && git commit -m "Initial commit"`);
+process.stdout.clearLine(0);
+process.stdout.cursorTo(0);
+process.stdout.write(`Initialized Git Repository.\n`);
 
 console.log(`\nStart with changing the directory:`);
 console.log(`cd ${name}`.green);
